9919eab5b61f32a0e12e907ca17c9a7833b08d9a
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-10-17 11:59:19 +0300

    Update sysinfo.py

--- test/sysinfo.py
+++ test/sysinfo.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import psutil
+import os
 import platform
 from .. import loader
 from hikkatl.types import Message
@@ -11,32 +11,25 @@ class ServerStatsMod(loader.Module):
 
     async def sysinfocmd(self, message: Message):
         uname = platform.uname()
-        cpu_count = psutil.cpu_count(logical=True)
-        cpu_freq = psutil.cpu_freq().current
-        cpu_usage = psutil.cpu_percent(interval=1)
-        mem = psutil.virtual_memory()
-        total_ram = mem.total / (1024 ** 3)
-        used_ram = mem.used / (1024 ** 3)
-        free_ram = mem.available / (1024 ** 3)
-        disk = psutil.disk_usage('/')
-        total_disk = disk.total / (1024 ** 3)
-        used_disk = disk.used / (1024 ** 3)
-        free_disk = disk.free / (1024 ** 3)
+        
+        total_ram = os.popen('cat /proc/meminfo | grep MemTotal').read().strip().split()[1]
+        used_ram = os.popen('cat /proc/meminfo | grep MemAvailable').read().strip().split()[1]
+        total_disk = os.popen('df -h / | awk \'\$1 == "Filesystem" {print $2}\'').read().strip()
+        used_disk = os.popen('df -h / | awk \'\$1 == "Filesystem" {print $3}\'').read().strip()
+        free_disk = os.popen('df -h / | awk \'\$1 == "Filesystem" {print $4}\'').read().strip()
 
         info = (
             f"<b>Системная информация:</b>\n"
             f"OS: {uname.system} {uname.release}\n"
-            f"CPU: {uname.processor} ({cpu_count} ядер)\n"
-            f"Частота CPU: {cpu_freq:.2f} MHz\n"
-            f"Загрузка CPU: {cpu_usage}%\n\n"
+            f"Архитектура: {uname.machine}\n"
+            f"Процессор: {uname.processor}\n\n"
             f"<b>Оперативная память:</b>\n"
-            f"Всего RAM: {total_ram:.2f} ГБ\n"
-            f"Использовано RAM: {used_ram:.2f} ГБ\n"
-            f"Свободно RAM: {free_ram:.2f} ГБ\n\n"
+            f"Всего RAM: {int(total_ram) / 1024:.2f} МБ\n"
+            f"Использовано RAM: {int(used_ram) / 1024:.2f} МБ\n\n"
             f"<b>Диск:</b>\n"
-            f"Всего: {total_disk:.2f} ГБ\n"
-            f"Использовано: {used_disk:.2f} ГБ\n"
-            f"Свободно: {free_disk:.2f} ГБ\n"
+            f"Всего: {total_disk}\n"
+            f"Использовано: {used_disk}\n"
+            f"Свободно: {free_disk}\n"
         )
 
         await message.edit(info)

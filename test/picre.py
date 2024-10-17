1bbda09d62a9d35162c7320eef73ed891175daf4
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-10-17 12:23:14 +0300

    Update picre.py

--- test/picre.py
+++ test/picre.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import aiohttp
-import json
 from .. import loader
 from hikkatl.types import Message
 
@@ -11,14 +10,14 @@ class RimMod(loader.Module):
 
     async def rimcmd(self, message: Message):
         url = "https://pic.re/image"
-        
+
         async with aiohttp.ClientSession() as session:
             async with session.post(url) as resp:
                 if resp.status == 200:
-                    data = await resp.json()  # Получаем ответ в формате JSON
+                    data = await resp.json()
                     img_url = data.get("file_url")
                     if img_url:
-                        await message.reply(img_url)
+                        await message.reply_photo(img_url)  # Отправляем изображение
                     else:
                         await message.reply("Не удалось найти изображение.")
                 else:

bdfa8ab55a1b75c2b822a6eac91f23da5798a109
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-10-17 12:19:01 +0300

    Update picre.py

--- test/picre.py
+++ test/picre.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import aiohttp
+import json
 from .. import loader
 from hikkatl.types import Message
 
@@ -12,18 +13,13 @@ class RimMod(loader.Module):
         url = "https://pic.re/image"
         
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
+            async with session.post(url) as resp:
                 if resp.status == 200:
-                    data = await resp.read()  # Чтение как байты
-                    img_url = self.extract_image_url(data.decode('utf-8', errors='ignore'))  # Декодирование с игнорированием ошибок
+                    data = await resp.json()  # Получаем ответ в формате JSON
+                    img_url = data.get("file_url")
                     if img_url:
                         await message.reply(img_url)
                     else:
                         await message.reply("Не удалось найти изображение.")
                 else:
                     await message.reply("Ошибка при получении изображения.")
-
-    def extract_image_url(self, html):
-        import re
-        match = re.search(r'<img src="(.*?)"', html)
-        return match.group(1) if match else None
